f620b602785a45b0c076e9f26e2bd55db88bb253
janczuramateusz7-eng <janczuramateusz7@gmail.com>
2026-04-25 11:55:51 +0200

    Update signInNavigation.spec.js

--- playwright-petstore-portfolio/tests/auth/signInNavigation.spec.js
+++ playwright-petstore-portfolio/tests/auth/signInNavigation.spec.js
@@ -1,1 +1,11 @@
+import { test, expect } from '@playwright/test';
 
+test('User can navigate to Sign In page from homepage', async ({ page }) => {
+  await page.goto('https://conduit.mate.academy', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  await page.getByRole('link', { name: 'Sign in' }).click();
+
+  await expect(page).toHaveURL('https://conduit.mate.academy/user/login');
+});
