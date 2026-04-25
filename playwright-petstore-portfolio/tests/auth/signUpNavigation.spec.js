5ba60381fc8bcb304d7f2d88d9b88d48e060e13f
janczuramateusz7-eng <janczuramateusz7@gmail.com>
2026-04-25 12:18:00 +0200

    Update signUpNavigation.spec.js

--- playwright-petstore-portfolio/tests/auth/signUpNavigation.spec.js
+++ playwright-petstore-portfolio/tests/auth/signUpNavigation.spec.js
@@ -1,11 +1,21 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
-test('User can navigate to Sign Up page from homepage', async ({ page }) => {
-  await page.goto('https://conduit.mate.academy', {
+test('User can register with valid data', async ({ page }) => {
+  await page.goto('https://conduit.mate.academy/user/register', {
     waitUntil: 'domcontentloaded'
   });
 
-  await page.getByRole('link', { name: 'Sign up' }).click();
+  const username = faker.person.firstName();
+  const email = faker.internet.email();
+  const password = faker.internet.password();
 
-  await expect(page).toHaveURL('https://conduit.mate.academy/user/register');
+  await page.getByPlaceholder('Username').fill(username);
+  await page.getByPlaceholder('Email').fill(email);
+  await page.getByPlaceholder('Password').fill(password);
+
+  await page.getByRole('button', { name: 'Sign up' }).click();
+
+  await expect(page).toHaveURL('https://conduit.mate.academy/');
+  await expect(page.getByText('Your Feed')).toBeVisible();
 });
