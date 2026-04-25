a389420771a71c30e63e2b69c9333e5de63f06f7
janczuramateusz7-eng <janczuramateusz7@gmail.com>
2026-04-25 12:23:42 +0200

    Update signUpForm.spec.js

--- playwright-petstore-portfolio/tests/registration/signUpForm.spec.js
+++ playwright-petstore-portfolio/tests/registration/signUpForm.spec.js
@@ -1,1 +1,16 @@
+import { test, expect } from '@playwright/test';
 
+test('Sign Up form elements are visible', async ({ page }) => {
+  await page.goto('https://conduit.mate.academy/user/register', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  await expect(page.getByRole('heading', { name: 'Sign up' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Have an account?' })).toBeVisible();
+
+  await expect(page.getByPlaceholder('Username')).toBeVisible();
+  await expect(page.getByPlaceholder('Email')).toBeVisible();
+  await expect(page.getByPlaceholder('Password')).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'Sign up' })).toBeVisible();
+});
