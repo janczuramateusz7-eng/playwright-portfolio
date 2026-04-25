6013908ac6b6235cd35ab9550f132930748cfabc
janczuramateusz7-eng <janczuramateusz7@gmail.com>
2026-04-25 12:18:21 +0200

    Update signInNavigation.spec.js

--- playwright-petstore-portfolio/tests/auth/signInNavigation.spec.js
+++ playwright-petstore-portfolio/tests/auth/signInNavigation.spec.js
@@ -1,11 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('User can navigate to Sign In page from homepage', async ({ page }) => {
-  await page.goto('https://conduit.mate.academy', {
+test('User can sign in with valid credentials', async ({ page }) => {
+  await page.goto('https://conduit.mate.academy/user/login', {
     waitUntil: 'domcontentloaded'
   });
 
-  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByPlaceholder('Email').fill('userqa@1.com');
+  await page.getByPlaceholder('Password').fill('Password1@');
 
-  await expect(page).toHaveURL('https://conduit.mate.academy/user/login');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+
+  await expect(page).toHaveURL('https://conduit.mate.academy/');
+  await expect(page.getByText('Your Feed')).toBeVisible();
 });
